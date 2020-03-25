6adf43cf82270adb281c94ff9eb10349acbe94be
Manuel Stingl <manuel@stingl.st>
2020-03-25 17:15:33 +0100

    Add colorspace converter to AbstractImageStorage

--- models.py
+++ models.py
@@ -64,7 +64,7 @@ class MozJPEGPostprocessor:
 
 
 class AbstractImageStorage(FileSystemStorage):
-    def __init__(self, *args, resize_to: tuple = None, resample=Image.BICUBIC, output_format='png', just_hash=False, hash_algo=hashlib.sha256, post_process=None, **kwargs):
+    def __init__(self, *args, resize_to: tuple = None, resample=Image.BICUBIC, output_format='png', just_hash=False, hash_algo=hashlib.sha256, post_process=None, should_fix_colorspace=True, **kwargs):
         """
         Image Storage.
         Resamples an Image before storing as File.
@@ -83,13 +83,49 @@ class AbstractImageStorage(FileSystemStorage):
         self.just_hash = just_hash
         self.hash_algo = hash_algo
         self.post_process = post_process
+        self.should_fix_colorspace = should_fix_colorspace
 
     def process_picture(self, file):
         raise NotImplementedError
 
+    @classmethod
+    def _fix_colorspace(cls, image, colorspace, output_format):
+        """
+        From https://github.com/jazzband/sorl-thumbnail/issues/564
+        """
+        if colorspace == 'RGB':
+            # Pillow JPEG doesn't allow RGBA anymore. It was converted to RGB before.
+            if image.mode == 'RGBA' and output_format != 'JPEG':
+                return image  # RGBA is just RGB + Alpha
+
+            if image.mode == 'LA' or (image.mode == 'P' and 'transparency' in image.info):
+                if output_format == 'JPEG':
+                    newimage = Image.new('RGB', image.size, '#ffffff')
+                    mask = image.convert('RGBA').split()[-1]
+                    newimage.paste(image.convert('RGBA'), (0, 0), mask)
+
+                else:
+                    newimage = image.convert('RGBA')
+                    transparency = image.info.get('transparency')
+                    if transparency is not None:
+                        mask = image.convert('RGBA').split()[-1]
+                        newimage.putalpha(mask)
+
+                return newimage
+
+            return image.convert('RGB')
+
+        if colorspace == 'GRAY':
+            return image.convert('L')
+
+        return image
+
     def _save(self, name, content):
         file = BytesIO()
         content.image = self.process_picture(content.file)
+        if self.should_fix_colorspace:
+            content.image = self._fix_colorspace(content.image, "RGB", self.format.upper())
+
         content.image.save(file, format=self.format)
 
         if self.post_process:
